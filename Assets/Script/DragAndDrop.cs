2d0f566a9fe71a1b6f68e79e3ee5c8320a8edf4c
EliasHaloun0104 <37835421+EliasHaloun0104@users.noreply.github.com>
2020-10-08 10:32:57 +0200

    Add DragAndDrop & PinchAndZoom

---
+++ Assets/Script/DragAndDrop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Camera originCamera;
+    private bool dragging = false;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originCamera = FindObjectOfType<Camera>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("MouseDown");
+        distance = Vector3.Distance(transform.position, originCamera.transform.position);
+        dragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("MouseUp");
+        dragging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = originCamera.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            rayPoint.z = 0;
+            transform.position = rayPoint;
+        }
+
+    }
+}
---
+++ Assets/Script/PinchAndZoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchAndZoom : MonoBehaviour
+{
+    private Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+
+
+    public void UpButton()
+    {
+        transform.position += Vector3.up;
+    }
+    
+    public void DownButton()
+    {
+        transform.position += Vector3.down;
+    }
+    
+    public void LeftButton()
+    {
+        transform.position += Vector3.left;
+    }
+    
+    public void RighButton()
+    {
+        transform.position += Vector3.right;
+    }
+
+    public void ZoomIn()
+    {
+        if(cam.orthographicSize > 3)
+            cam.orthographicSize --;
+    }
+    
+    public void ZoomOut()
+    {
+        if(cam.orthographicSize <10)
+            cam.orthographicSize ++;
+    }
+    
+}
