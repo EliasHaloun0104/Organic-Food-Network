864d367455d624c29098fb0ac3cdef5d966a7f4e
EliasHaloun0104 <37835421+EliasHaloun0104@users.noreply.github.com>
2020-10-19 11:01:46 +0200

    Edit Naming

---
+++ Assets/Script/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public partial class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string Address { get; set; }
+    public float XCoordinate { get; set; }
+    public float YCoordinate { get; set; }
+    public string Role { get; set; }
+    
+}
